0143f33fa4fd1648052084ddf3066c773fbe1202
Steven Tomcavage <steven@tomcavage.com>
2011-12-14 22:34:53 -0500

    Added code to parse JSON config file, which required a rewrite of some of the data types. Also, all of the data types were renamed to make them unique.

--- src/Types.hs
+++ src/Types.hs
@@ -1,42 +1,88 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- Note: the JSON parsing code is based on code found at 
+-- http://stackoverflow.com/questions/6930944/haskell-aeson-json-parsing-into-custom-type
+
 module Types (
-    -- list exported types here
+
+    -- Exported data types
+    AdvRoom,
+    AdvExit,
+    AdvLocation,
+    AdvObject,
+    AdvPlayer,
+    AdvMonster,
+    AdvConfig
+
 ) where 
 
--- | How to move between locations
-data Exits = Exit {
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.TH (deriveJSON)
+import Data.Attoparsec
+import qualified Data.ByteString as B
+
+data AdvRoom = AdvRoom {
+    roomName  :: String,
+    roomDesc  :: String
+} deriving (Show)
+
+deriveJSON id ''AdvRoom
+
+-- | How to move between locations in a game
+data AdvExit = AdvExit {
     exitDir :: String,
-    exitTo  :: Location
-}
+    exitTo  :: AdvRoom
+} deriving (Show)
+
+deriveJSON id ''AdvExit
 
 -- | The basic layout for a room
-data Location = Loc {
-    locName  :: String,
-    locDesc  :: String,
-    locExits :: [Exits]
-}
+data AdvLocation = AdvLocation {
+    locRoom  :: AdvRoom,
+    locExits :: [AdvExit]
+} deriving (Show)
 
--- | The whole world for the game
-data AdvMap = AdvMap { 
-    mapLocations :: [Location] 
-}
+deriveJSON id ''AdvLocation
 
 -- | Things that can be moved and equiped
-data Object = Obj {
+data AdvObject = AdvObject {
     objName    :: String,
     objDesc    :: String,
     objPickup  :: String,
     objDiscard :: String
-}
+} deriving (Show)
+
+deriveJSON id ''AdvObject
 
 -- | The character that the end user is playing
-data Player = Player {
-    playerAt  :: Location,
-    playerHas :: [Object] 
-}
+data AdvPlayer = AdvPlayer {
+    playerAt  :: AdvLocation,
+    playerHas :: [AdvObject] 
+} deriving (Show)
+
+deriveJSON id ''AdvPlayer
 
 -- | All the non-player characters in the game
-data Monster = Monster {
+data AdvMonster = AdvMonster {
     monsterDesc :: String,
-    monsterAt   :: Location,
-    monsterHas  :: [Object]
-}
+    monsterAt   :: AdvLocation,
+    monsterHas  :: [AdvObject]
+} deriving (Show)
+
+deriveJSON id ''AdvMonster
+
+-- | The config file used to load and save the game state
+data AdvConfig = AdvConfig {
+    locations :: [AdvLocation],
+    objects   :: [AdvObject],
+    players   :: [AdvPlayer],
+    monsters  :: [AdvMonster]    
+} deriving (Show)
+
+deriveJSON id ''AdvConfig
+
+parseAdvConfigFile = fmap parseAdvConfigData . B.readFile
+
+parseAdvConfigData :: B.ByteString -> Data.Attoparsec.Result (Data.Aeson.Result [AdvConfig])
+parseAdvConfigData content = parse (fmap fromJSON json) content
