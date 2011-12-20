3eb445ebe4383846b78b38267e16daf079716a41
Steven Tomcavage <steven@tomcavage.com>
2011-12-20 01:19:23 -0500

    Fleshing out the command line parser

--- src/Main.hs
+++ src/Main.hs
@@ -31,20 +31,30 @@ cleanTest = "cleanTest" ~: TestList
       clean ["the","quick","brown","fox"] ~?= ["quick","brown","fox"],
       clean ["a,","to","the","z"]         ~?= ["a,","z"] ]
 
--- | Convert a user's command into an Action in the game
+-- Handle verbs that act on a Dir
+-- TODO: remove code duplication
 parseDir :: String -> [String] -> Maybe Action
-parseDir "go" [objects] = 
-    case [objects] of
-        ("north":_) -> Just (Go N)
-        ("south":_) -> Just (Go S)
-        ("east":_)  -> Just (Go E)
-        ("west":_)  -> Just (Go W)
-        ("up":_)    -> Just (Go U)
-        ("down":_)  -> Just (Go D)
-        _           -> Nothing
+parseDir verb [objects] = 
+    case verb of 
+        "go" -> case [objects] of
+            ("north":_) -> Just (Go N)
+            ("south":_) -> Just (Go S)
+            ("east":_)  -> Just (Go E)
+            ("west":_)  -> Just (Go W)
+            ("up":_)    -> Just (Go U)
+            ("down":_)  -> Just (Go D)
+            _           -> Nothing
+        "rmdoor" -> case [objects] of
+            ("north":_) -> Just (RmDoor N)
+            ("south":_) -> Just (RmDoor S)
+            ("east":_)  -> Just (RmDoor E)
+            ("west":_)  -> Just (RmDoor W)
+            ("up":_)    -> Just (RmDoor U)
+            ("down":_)  -> Just (RmDoor D)
+            _           -> Nothing
 parseDir _ _ = Nothing
 
--- | Convert a user's request about an object into an Action in the game
+-- | Handle verbs that act on an AdvObject
 -- | TODO: check with game inventory to ensure object exists before acting on it
 parseAdvObject :: String -> [String] -> Maybe Action
 parseAdvObject verb (x:_) = 
@@ -57,37 +67,58 @@ parseAdvObject verb (x:_) =
     where o = mkObj x ""
 parseAdvObject _ _ = Nothing
 
--- | Convert a user's request to create a bag into an Action in the game
+-- | Handle verbs that act on a Bag
 parseBag :: String -> [String] -> Maybe Action
 parseBag "mkbag" (x:_) = Just (MkBag (Bag x "" []))
 parseBag _ _           = Nothing
 
--- | Convert a user's request to create a player into an Action in the game
+-- | Handle verbs that act on a Player
 parsePlayer :: String -> [String] -> Maybe Action
 parsePlayer "mkplayer" (x:_) = 
     Just (MkPlayer (Player x "" M.empty [] True False))
 parsePlayer _ _ = Nothing
+
+-- | Handle verbs that act on a string
+-- | TODO: check with game inventory to ensure object exists before acting on it
+parseString :: String -> [String] -> Maybe Action
+parseString verb (x:_) = 
+    case verb of
+        "rmobj" -> Just (RmObj x)
+        _       -> Nothing
+parseString _ _ = Nothing
+
+-- | Handle verbs that act on a room and a string
+parseRoomString :: String -> [String] -> Maybe Action
+parseRoomString verb (x:xs) = 
+    case verb of
+        "teleports" -> Just (Teleports (Room x "" M.empty []) (unwords xs))
+        _           -> Nothing
+parseRoomString _ _ = Nothing
 
 -- | Convert the user's text input into an action the game can execute
 -- | TODO: create a map of allowable actions and their parsing functions to
 -- |       make the code more generic
 parse :: String -> Maybe Action
 parse input = case verb of
-    "go"       -> parseDir verb objects
-    "get"      -> parseAdvObject verb objects
-    "drop"     -> parseAdvObject verb objects
-    "use"      -> parseAdvObject verb objects
-    "mkobj"    -> parseAdvObject verb objects
-    "mkrm"     -> error "undefined" -- TODO: complete this action
-    "mkbag"    -> parseBag verb objects
-    "mkplayer" -> parsePlayer verb objects
-    "say"      -> Just (Say (unwords $ drop 1 $ words input))
-    "yell"     -> Just (Yell (unwords $ drop 1 $ words input))
-    "inv"      -> Just Inv
-    "stats"    -> Just Stats
-    "save"     -> Just Save
-    "quit"     -> Just Quit
-    _          -> Nothing
+    "go"        -> parseDir verb objects
+    "get"       -> parseAdvObject verb objects
+    "drop"      -> parseAdvObject verb objects
+    "use"       -> parseAdvObject verb objects
+    "mkobj"     -> parseAdvObject verb objects
+    "mkrm"      -> error "undefined" -- TODO: complete this action
+    "mkbag"     -> parseBag verb objects
+    "mkplayer"  -> parsePlayer verb objects
+    "rmobj"     -> parseString verb objects
+    "rmdoor"    -> parseDir verb objects
+    "setstat"   -> error "undefined" -- TODO: complete this action
+    "teleports" -> error "undefined" -- TODO: complete this action
+    "say"       -> Just (Say (unwords $ drop 1 $ words input))
+    "yell"      -> Just (Yell (unwords $ drop 1 $ words input))
+    "inv"       -> Just Inv
+    "stats"     -> Just Stats
+    "save"      -> Just Save
+    "quit"      -> Just Quit
+    _           -> Nothing
     where (verb:objects) = clean (tokenize input)
 
 testHammer = mkObj "hammer" ""
@@ -103,6 +134,8 @@ parseTest = "parseTest" ~: TestList
       parse "Use hammer"            ~?= Just (Use testHammer),
       parse "MkObj hammer"          ~?= Just (MkObj testHammer),
       parse "MkBag myBag"           ~?= Just (MkBag testBag),
+      parse "RmObj sticks & stones" ~?= Just (RmObj "sticks"),
+      parse "RmDoor east"           ~?= Just (RmDoor E),
       parse "Say this is the time"  ~?= Just (Say "this is the time"),
       parse "Yell yodel-eh-he-hoo!" ~?= Just (Yell "yodel-eh-he-hoo!")
     ]    
