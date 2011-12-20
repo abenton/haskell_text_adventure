bbefa1c76f9e888f881e9e20024752963e8b6e39
Steven Tomcavage <steven@tomcavage.com>
2011-12-20 00:18:23 -0500

    More parsing code

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Builder
 import Char
+import Data.Map as M
 import Test.HUnit
 import Types2
 
@@ -12,7 +13,7 @@ main = do
 
 -- | Convert a command line input into a list of tokens, separated by spaces
 tokenize :: String -> [String]
-tokenize = words . map toLower
+tokenize = words . Prelude.map toLower
        
 tokenizeTest :: Test
 tokenizeTest = "tokenizeTest" ~: TestList 
@@ -22,7 +23,7 @@ tokenizeTest = "tokenizeTest" ~: TestList
 
 -- | Remove incidental words from token list
 clean :: [String] -> [String]
-clean = filter (`notElem` ["a","the","to"])
+clean = Prelude.filter (`notElem` ["a","the","to"])
 
 cleanTest :: Test
 cleanTest = "cleanTest" ~: TestList
@@ -30,6 +31,7 @@ cleanTest = "cleanTest" ~: TestList
       clean ["the","quick","brown","fox"] ~?= ["quick","brown","fox"],
       clean ["a,","to","the","z"]         ~?= ["a,","z"] ]
 
+-- | Convert a user's command into an Action in the game
 parseDir :: String -> [String] -> Maybe Action
 parseDir "go" [objects] = 
     case [objects] of
@@ -42,47 +44,65 @@ parseDir "go" [objects] =
         _           -> Nothing
 parseDir _ _ = Nothing
 
+-- | Convert a user's request about an object into an Action in the game
+-- | TODO: check with game inventory to ensure object exists before acting on it
 parseAdvObject :: String -> [String] -> Maybe Action
-parseAdvObject verb (object:_) = 
+parseAdvObject verb (x:_) = 
     case verb of
         "get"   -> Just (Get o)
         "drop"  -> Just (Drop o)
         "use"   -> Just (Use o)
         "mkobj" -> Just (MkObj o)
         _       -> Nothing
-    where o = mkObj object ""
+    where o = mkObj x ""
 parseAdvObject _ _ = Nothing
+
+-- | Convert a user's request to create a bag into an Action in the game
+parseBag :: String -> [String] -> Maybe Action
+parseBag "mkbag" (x:_) = Just (MkBag (Bag x "" []))
+parseBag _ _           = Nothing
+
+-- | Convert a user's request to create a player into an Action in the game
+parsePlayer :: String -> [String] -> Maybe Action
+parsePlayer "mkplayer" (x:_) = 
+    Just (MkPlayer (Player x "" M.empty [] True False))
+parsePlayer _ _ = Nothing
 
 -- | Convert the user's text input into an action the game can execute
 -- | TODO: create a map of allowable actions and their parsing functions to
 -- |       make the code more generic
 parse :: String -> Maybe Action
 parse input = case verb of
-    "go"    -> parseDir verb objects
-    "get"   -> parseAdvObject verb objects
-    "drop"  -> parseAdvObject verb objects
-    "use"   -> parseAdvObject verb objects
-    "mkobj" -> parseAdvObject verb objects
-    "say"   -> Just (Say (unwords $ drop 1 $ words input))
-    "yell"  -> Just (Yell (unwords $ drop 1 $ words input))
-    "inv"   -> Just Inv
-    "stats" -> Just Stats
-    "save"  -> Just Save
-    "quit"  -> Just Quit
-    _       -> Nothing
+    "go"       -> parseDir verb objects
+    "get"      -> parseAdvObject verb objects
+    "drop"     -> parseAdvObject verb objects
+    "use"      -> parseAdvObject verb objects
+    "mkobj"    -> parseAdvObject verb objects
+    "mkrm"     -> error "undefined" -- TODO: complete this action
+    "mkbag"    -> parseBag verb objects
+    "mkplayer" -> parsePlayer verb objects
+    "say"      -> Just (Say (unwords $ drop 1 $ words input))
+    "yell"     -> Just (Yell (unwords $ drop 1 $ words input))
+    "inv"      -> Just Inv
+    "stats"    -> Just Stats
+    "save"     -> Just Save
+    "quit"     -> Just Quit
+    _          -> Nothing
     where (verb:objects) = clean (tokenize input)
 
 testHammer = mkObj "hammer" ""
+testBag    = Bag "mybag" "" []
 
 parseTest :: Test
 parseTest = "parseTest" ~: TestList
     [ parse "Dude, where's my car?" ~?= Nothing,
       parse "Go West"               ~?= Just (Go W),
       parse "Go westerly"           ~?= Nothing,
-      parse "Get hammer"            ~?= Just (Get testHammer ),
-      parse "Drop hammer"           ~?= Just (Drop testHammer ),
-      parse "Use hammer"            ~?= Just (Use testHammer ),
-      parse "MkObj hammer"          ~?= Just (MkObj testHammer ),
+      parse "Get hammer"            ~?= Just (Get testHammer),
+      parse "Drop hammer"           ~?= Just (Drop testHammer),
+      parse "Use hammer"            ~?= Just (Use testHammer),
+      parse "MkObj hammer"          ~?= Just (MkObj testHammer),
+      parse "MkBag myBag"           ~?= Just (MkBag testBag),
       parse "Say this is the time"  ~?= Just (Say "this is the time"),
       parse "Yell yodel-eh-he-hoo!" ~?= Just (Yell "yodel-eh-he-hoo!")
     ]    
