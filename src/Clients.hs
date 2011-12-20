96734181f527cf30d67784b2c59f9fa21ed97a4d
Adrian Benton <adrian.donald.benton@gmail.com>
2011-12-20 12:56:40 -0500

    Added grumpy gnome client.

--- src/Clients.hs
+++ src/Clients.hs
@@ -1,9 +1,28 @@
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults #-}
 
-module Clients where
+module Clients (annoyingNPCThread, main) where
+import Builder (mkPlayer)
+import Types2 (Player, Thing(name))
+import Network (withSocketsDo, connectTo, PortID(..))
+import System.IO (hPutStrLn, Handle)
+import Control.Concurrent (forkIO, threadDelay)
 
--- | Contains definitions for clients along with some simple behaviors/ways of
--- | composing behaviors.
+-- | Contains definitions for NPC clients.
+main :: IO ()
+main = withSocketsDo $ do
+       h <- connectTo "localhost" $ PortNumber 8080
+       _ <- forkIO $ annoyingNPCThread gnome "yell Kids these days!" h
+       return ()
 
-annoyingNPCThread :: IO ()
-annoyingNPCThread = error "undefined"
+gnome :: Player
+gnome = mkPlayer npcName npcDesc False where
+  npcName = "grumpy gnome"
+  npcDesc = "This gnome comes with a permanent frown."
+
+-- | Periodically yells nonsense to localhost.
+annoyingNPCThread :: Player -> String -> Handle -> IO ()
+annoyingNPCThread p s h = do _ <- hPutStrLn h ("addme " ++ name p) 
+                             loop where
+                               loop = do _ <- threadDelay 5000
+                                         _ <- hPutStrLn h s
+                                         loop
