a701576c2bbcfbc65378a2d403d036942832b2d3
Steven Tomcavage <steven@tomcavage.com>
2011-12-19 23:51:44 -0500

    More command-line parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Builder
 import Char
 import Test.HUnit
 import Types2
@@ -29,36 +30,60 @@ cleanTest = "cleanTest" ~: TestList
       clean ["the","quick","brown","fox"] ~?= ["quick","brown","fox"],
       clean ["a,","to","the","z"]         ~?= ["a,","z"] ]
 
--- | Convert the user-supplied direction into the corresponding Dir type
-parseDir :: [String] -> Maybe Action
-parseDir [objects] = case [objects] of
-    ("north":_) -> Just (Go N)
-    ("south":_) -> Just (Go S)
-    ("east":_)  -> Just (Go E)
-    ("west":_)  -> Just (Go W)
-    ("up":_)    -> Just (Go U)
-    ("down":_)  -> Just (Go D)
-    _           -> Nothing
+parseDir :: String -> [String] -> Maybe Action
+parseDir "go" [objects] = 
+    case [objects] of
+        ("north":_) -> Just (Go N)
+        ("south":_) -> Just (Go S)
+        ("east":_)  -> Just (Go E)
+        ("west":_)  -> Just (Go W)
+        ("up":_)    -> Just (Go U)
+        ("down":_)  -> Just (Go D)
+        _           -> Nothing
+parseDir _ _ = Nothing
 
-parseAdvObject :: [String] -> Maybe Action
-parseAdvObject [objects] = error "parseAdvObject is not yet defined"
+parseAdvObject :: String -> [String] -> Maybe Action
+parseAdvObject verb (object:_) = 
+    case verb of
+        "get"   -> Just (Get o)
+        "drop"  -> Just (Drop o)
+        "use"   -> Just (Use o)
+        "mkobj" -> Just (MkObj o)
+        _       -> Nothing
+    where o = mkObj object ""
+parseAdvObject _ _ = Nothing
 
 -- | Convert the user's text input into an action the game can execute
+-- | TODO: create a map of allowable actions and their parsing functions to
+-- |       make the code more generic
 parse :: String -> Maybe Action
 parse input = case verb of
-    "go"   -> parseDir objects
-    "get"  -> parseAdvObject objects
-    "drop" -> parseAdvObject objects
-    "use"  -> parseAdvObject objects
-    "say"  -> Just (Say (unwords $ drop 1 $ words input))
-    "yell" -> Just (Yell (unwords $ drop 1 $ words input))
-    _    -> Nothing
+    "go"    -> parseDir verb objects
+    "get"   -> parseAdvObject verb objects
+    "drop"  -> parseAdvObject verb objects
+    "use"   -> parseAdvObject verb objects
+    "mkobj" -> parseAdvObject verb objects
+    "say"   -> Just (Say (unwords $ drop 1 $ words input))
+    "yell"  -> Just (Yell (unwords $ drop 1 $ words input))
+    "inv"   -> Just Inv
+    "stats" -> Just Stats
+    "save"  -> Just Save
+    "quit"  -> Just Quit
+    _       -> Nothing
     where (verb:objects) = clean (tokenize input)
-    
+
+testHammer = mkObj "hammer" ""
+
 parseTest :: Test
 parseTest = "parseTest" ~: TestList
-    [ parse "Go West" ~?= Just (Go W),
-      parse "Say this is the time" ~?= Just (Say "this is the time"),
+    [ parse "Dude, where's my car?" ~?= Nothing,
+      parse "Go West"               ~?= Just (Go W),
+      parse "Go westerly"           ~?= Nothing,
+      parse "Get hammer"            ~?= Just (Get testHammer ),
+      parse "Drop hammer"           ~?= Just (Drop testHammer ),
+      parse "Use hammer"            ~?= Just (Use testHammer ),
+      parse "MkObj hammer"          ~?= Just (MkObj testHammer ),
+      parse "Say this is the time"  ~?= Just (Say "this is the time"),
       parse "Yell yodel-eh-he-hoo!" ~?= Just (Yell "yodel-eh-he-hoo!")
     ]    
     
